456fd0f7e10b3571f95785e4231136ff3868ce96
PhillipSegura <PhillipaSegura@gmail.com>
2024-12-16 13:48:54 -0700

    compeleted with help from class mates

---
+++ fashion-blog/src/components/Header.jsx
@@ -0,0 +1,32 @@
+function Header() {
+  return (
+    <header>
+      <h1 className="title">Sartre's List</h1>
+      <h2 className="sub-title">Better-Dressed People</h2>
+      <nav aria-label="Main Navigation" role="navigation">
+        <ul className="nav-UL">
+          <li>
+            <a href="">Women's</a>
+          </li>
+          <li>
+            <a href="">Men's</a>
+          </li>
+          <li>
+            <a href="">On the Steet</a>
+          </li>
+          <li>
+            <a href="">The Catwalk</a>
+          </li>
+          <li>
+            <a href="">AdWatch</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ fashion-blog/src/components/Main.jsx
@@ -0,0 +1,49 @@
+function Main() {
+  return (
+    <main>
+      <article>
+        <h3>11/12/20</h3>
+        <h1 id="head-line">On the Street in Brooklyn</h1>
+        <img src="../public/blog-image-1.jpg" alt="" />
+        <p>
+          <span>C</span>ray Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Nihil veniam dicta error rem repellendus iusto voluptatem at
+          consequuntur, mollitia quo incidunt amet numquam autem tempore iure
+          deserunt minima dolore velit. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Dolore illo porro laboriosam. Quod vitae dolorem
+          nihil cumque nobis tenetur laboriosam, ea deserunt veniam quam iure
+          nisi sint neque odio recusandae! Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Sit in laborum provident adipisci
+          consequatur, aspernatur voluptate a, placeat atque qui animi ipsa vero
+          laudantium nostrum deserunt eveniet vitae rerum ullam.
+        </p>
+        <div>
+          <a href="">Continues...</a>
+        </div>
+      </article>
+
+      <article>
+        <h3>11/11/20</h3>
+        <h1 id="head-line">Vintage in Vogue</h1>
+        <img src="../public/blog-image-2.jpg" alt="" />
+        <p>
+          <span>S</span>ray Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Nihil veniam dicta error rem repellendus iusto voluptatem at
+          consequuntur, mollitia quo incidunt amet numquam autem tempore iure
+          deserunt minima dolore velit. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Dolore illo porro laboriosam. Quod vitae dolorem
+          nihil cumque nobis tenetur laboriosam, ea deserunt veniam quam iure
+          nisi sint neque odio recusandae! Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Sit in laborum provident adipisci
+          consequatur, aspernatur voluptate a, placeat atque qui animi ipsa vero
+          laudantium nostrum deserunt eveniet vitae rerum ullam.
+        </p>
+        <div>
+          <a href="">Continues...</a>
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export default Main;
